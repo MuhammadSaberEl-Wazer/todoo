b0c15cead967706640dcdad0f7408a016770908b
MuhammadSaberEl-Wazer <muhammadsaberit@gmail.com>
2022-02-22 12:30:25 +0200

    Updates

---
+++ precache-manifest.93dc56e9a00b9fcd206021b582c5b3d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1bf60ecada9e17306099c3bd0f0235ce",
+    "url": "%7BMuhammadSaberEl-Wazer%7D.github.io/%7Bmasarefone%7D/index.html"
+  },
+  {
+    "revision": "95dbee277a857836927e",
+    "url": "%7BMuhammadSaberEl-Wazer%7D.github.io/%7Bmasarefone%7D/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "67fde30e9a5b3f94b571",
+    "url": "%7BMuhammadSaberEl-Wazer%7D.github.io/%7Bmasarefone%7D/static/css/main.29fe2d4d.chunk.css"
+  },
+  {
+    "revision": "95dbee277a857836927e",
+    "url": "%7BMuhammadSaberEl-Wazer%7D.github.io/%7Bmasarefone%7D/static/js/2.bde1e388.chunk.js"
+  },
+  {
+    "revision": "67fde30e9a5b3f94b571",
+    "url": "%7BMuhammadSaberEl-Wazer%7D.github.io/%7Bmasarefone%7D/static/js/main.2a0c679a.chunk.js"
+  },
+  {
+    "revision": "098a8e4d4517472a11eb",
+    "url": "%7BMuhammadSaberEl-Wazer%7D.github.io/%7Bmasarefone%7D/static/js/runtime~main.d3958489.js"
+  }
+]);
